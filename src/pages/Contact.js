a9f1c6faf804e33cee3311583b0618d400ce78cc
Dimitrij Fedoryno <dfedoryno@gmail.com>
2024-07-26 14:52:40 +0200

    footer and contact edit

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -4,7 +4,7 @@ const Contact = () => {
   return (
     <div className="container mx-auto p-4">
       <h1 className="text-3xl font-bold mb-4">Kontakt</h1>
-      <p>Kontaktujte nás na emailové adrese info@example.com.</p>
+      <p>Kontaktujte nás na emailové adrese dimitrij@fedoryno.cz</p>
     </div>
   );
 };
